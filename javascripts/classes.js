d34a88ca6aacf5e7e1b2c44506bf1ca1d994b216
Damon Romano <rdromano@mac.com>
2016-05-20 15:46:03 -0500

    Added stealthUserClasses.js to fill out classes in the index.html (thief, ninja, and assassin) that didn't point to anything. Rectified stats for Fighters and Magic-users as well.

--- javascripts/classes.js
+++ javascripts/classes.js
@@ -20,48 +20,38 @@ var Gauntlet = (function(muscles) {
   };
 
     muscles.GuildHall.Fighter = function() {
-      this.healthBonus = 20;
-      this.strengthBonus = 10;
+      this.healthBonus = this.healthBonus + 20;
+      this.strengthBonus = this.strengthBonus + 15;
     };
     muscles.GuildHall.Fighter.prototype = new Gauntlet.GuildHall.PlayerClass();
 
-    muscles.GuildHall.Warrior = function() {
-      this.name = "Warrior";
-      this.healthBonus = this.healthBonus + 25;
-      this.strengthBonus = this.strengthBonus + 30;
-    };
-    muscles.GuildHall.Warrior.prototype = new Gauntlet.GuildHall.Fighter();
+      muscles.GuildHall.Warrior = function() {
+        this.name = "Warrior";
+        this.healthBonus = this.healthBonus + 25;
+        this.strengthBonus = this.strengthBonus + 30;
+      };
+      muscles.GuildHall.Warrior.prototype = new Gauntlet.GuildHall.Fighter();
 
-    muscles.GuildHall.Valkyrie = function() {
-      this.name = "Valkyrie";
-      this.healthBonus = this.healthBonus + 20;
-      this.strengthBonus = this.strengthBonus + 10;
-    };
-    muscles.GuildHall.Valkyrie.prototype = new Gauntlet.GuildHall.Fighter();
+      muscles.GuildHall.Valkyrie = function() {
+        this.name = "Valkyrie";
+        this.healthBonus = this.healthBonus + 20;
+        this.strengthBonus = this.strengthBonus + 10;
+      };
+      muscles.GuildHall.Valkyrie.prototype = new Gauntlet.GuildHall.Fighter();
 
-    muscles.GuildHall.Berserker = function() {
-      this.name = "Berserker";
-      this.healthBonus = this.healthBonus + 35;
-      this.strengthBonus = this.strengthBonus + 20;
-    };
-    muscles.GuildHall.Berserker.prototype = new Gauntlet.GuildHall.Fighter();
+      muscles.GuildHall.Berserker = function() {
+        this.name = "Berserker";
+        this.healthBonus = this.healthBonus + 35;
+        this.strengthBonus = this.strengthBonus + 20;
+      };
+      muscles.GuildHall.Berserker.prototype = new Gauntlet.GuildHall.Fighter();
 
-    muscles.GuildHall.Monk = function() {
-      this.name = "Monk";
-      this.healthBonus = this.healthBonus + 10;
-      this.strengthBonus = this.strengthBonus + 40;
-    };
-    muscles.GuildHall.Monk.prototype = new Gauntlet.GuildHall.Fighter();
+      muscles.GuildHall.Monk = function() {
+        this.name = "Monk";
+        this.healthBonus = this.healthBonus + 10;
+        this.strengthBonus = this.strengthBonus + 40;
+      };
+      muscles.GuildHall.Monk.prototype = new Gauntlet.GuildHall.Fighter();
 
   return muscles;
 })(Gauntlet || {})
-
-
-
-/*
-    STEALTH CLASSES
-      - Thief
-      - Ninja
-      - Assassin
- */
-
