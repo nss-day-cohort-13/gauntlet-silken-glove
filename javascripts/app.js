e5978ecc8fc463e74a8f8717c0ce85e92f6ec403
Chase Ramsey <ramsey.chase@gmail.com>
2016-05-20 14:37:16 -0500

    Fixed issue with function calls after IIFE-izing

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,13 +2,13 @@
   Test code to generate a human player and an orc player
  */
 var warrior = new Gauntlet.Combatants.Human();
-warrior.setWeapon(new WarAxe());
+warrior.setWeapon(new Gauntlet.WarAxe());
 warrior.generateClass();  // This will be used for "Surprise me" option
 console.log(warrior.toString());
 
 var orc = new Gauntlet.Combatants.Orc();
 orc.generateClass();
-orc.setWeapon(new BroadSword());
+orc.setWeapon(new Gauntlet.BroadSword());
 console.log(orc.toString());
 
 /*
