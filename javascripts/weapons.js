c0d972c6c790009db5a3fb2435e88af33113cb30
Damon Romano <rdromano@mac.com>
2016-05-20 13:30:11 -0500

    Lift Magic Using classes to their own .js file. Commented out all magic using classes, all weapons except 'Bare Hands', and all fighter classes except 'Beggar' for MVP purposes.

--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -8,24 +8,24 @@ var Weapon = function() {
   }
 };
 
-var Dagger = function() {
-  this.name = "dagger";
-  this.damage = 4;
-  this.hands = 1;
-};
-Dagger.prototype = new Weapon();
+// var Dagger = function() {
+//   this.name = "dagger";
+//   this.damage = 4;
+//   this.hands = 1;
+// };
+// Dagger.prototype = new Weapon();
 
-var BroadSword = function() {
-  this.name = "broad sword";
-  this.damage = 14;
-  this.hands = 2;
-};
-BroadSword.prototype = new Weapon();
+// var BroadSword = function() {
+//   this.name = "broad sword";
+//   this.damage = 14;
+//   this.hands = 2;
+// };
+// BroadSword.prototype = new Weapon();
 
-var WarAxe = function() {
-  this.name = "war axe";
-  this.damage = 18;
-  this.hands = 2;
-};
-WarAxe.prototype = new Weapon();
+// var WarAxe = function() {
+//   this.name = "war axe";
+//   this.damage = 18;
+//   this.hands = 2;
+// };
+// WarAxe.prototype = new Weapon();
 
