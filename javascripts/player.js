f9dda96fb5f6668014a050306a9b24f1fe3dcb07
Chase Ramsey <ramsey.chase@gmail.com>
2016-05-21 13:45:51 -0500

    Edited Gauntlet.Player and began new Aristocrat and Peasant species

--- javascripts/player.js
+++ javascripts/player.js
@@ -12,82 +12,53 @@ Gauntlet.Combatants.Player = function(name) {
   this.species = null;
   this.class = null;
   this.weapon = null;
-
   this.name = this.name || "unknown adventurer";
   this.health = Math.floor(Math.random() * 40 + 50);
-  this.limbs = ["head", "neck", "arm", "leg", "torso"];
-  this.skinColor = "gray";
-  this.skinColors = [this.skinColor];
-  this.strength = 90;
-  this.intelligence = 90;
-
-  this.toString = function() {
-    var output = [this.playerName,
-      ": a ",
-      this.skinColor,
-      " skinned ",
-      this.species,
-      " ",
-      this.class,
-      " with ",
-      this.health,
-      " health. ",
-      (this.class.magical) ? "Able to cast " : " Wielding a ",
-      this.weapon.toString(),
-      "!"
-    ].join("");
-    return output;
-  };
 };
 
 Gauntlet.Combatants.Player.prototype.setWeapon = function(newWeapon) {
   this.weapon = newWeapon;
 }
 
-Gauntlet.Combatants.Player.prototype.generateClass = function() {
-  // Get a random index from the allowed classes array
-  var random = Math.round(Math.random() * (this.allowedClasses.length - 1));
+// Gauntlet.Combatants.Player.prototype.generateClass = function() {
+//   // Get a random index from the allowed classes array
+//   var random = Math.round(Math.random() * (this.allowedClasses.length - 1));
 
-  // Get the string at the index
-  var randomClass = this.allowedClasses[random];
+//   // Get the string at the index
+//   var randomClass = this.allowedClasses[random];
 
-  // Composes the corresponding player class into the player object
-  this.class = new Gauntlet.GuildHall[randomClass]();
+//   // Composes the corresponding player class into the player object
+//   this.class = new Gauntlet.GuildHall[randomClass]();
 
-  // Add the health bonus
-  this.health += this.class.healthBonus;
-  return this.class;
-};
+//   // Add the health bonus
+//   this.health += this.class.healthBonus;
+//   return this.class;
+// };
 
 /*
   Define the base properties for a human in a
   constructor function.
  */
-Gauntlet.Combatants.Human = function() {
+Gauntlet.Combatants.Aristocrat = function() {
   var randomSkin;
-
-  this.species = "Human";
-  this.intelligence = this.intelligence + 20;
-
-  this.skinColors.push("brown", "red", "white", "disease");
-  randomSkin = Math.round(Math.random() * (this.skinColors.length-1));
-  this.skinColor = this.skinColors[randomSkin];
-
-  this.allowedClasses = ["Warrior", "Berserker", "Valkyrie", "Monk"];
+  this.species = "Aristocrat";
+  this.vigor = 90;
+  this.potency = 90;
+  this.richesse = 90;
 };
-Gauntlet.Combatants.Human.prototype = new Gauntlet.Combatants.Player();
+Gauntlet.Combatants.Aristocrat.prototype = new Gauntlet.Combatants.Player();
 
 
 /*
   Define the base properties for a monster in a
   constructor function.
  */
-Gauntlet.Combatants.Monster = function() {
+Gauntlet.Combatants.Peasant = function() {
   this.health = this.health - 30;
-  this.intelligence = this.intelligence -20;
-  this.strength = this.strength + 30;
-  this.name = "Enemy";
+  this.filth = 90;
+  this.hunger = 90;
+  this.poverty = 90;
 };
 
-Gauntlet.Combatants.Monster.prototype = new Gauntlet.Combatants.Player();
+Gauntlet.Combatants.Peasant.prototype = new Gauntlet.Combatants.Player();
 
