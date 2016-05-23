1f7d471d0ed94dfaad70cf451867d56311ef1c0a
Chase Ramsey <ramsey.chase@gmail.com>
2016-05-23 13:10:53 -0500

    Added all stats to print

--- javascripts/combat.js
+++ javascripts/combat.js
@@ -10,13 +10,23 @@ battle.combatSetup = function(userClass, userName, userWeapon) {
     player.class = new Gauntlet.GuildHall[userClass];
     player.weapon = new Gauntlet[userWeapon];
     $("#player_stats").html(`<p>adventurer: ${player.name}</p> <p>class: ${player.class.name}</p>
-      <p>species: ${player.species}</p> <p>health: ${player.health}</p> <p>strength: ${player.strength}</p>
-      <p>weapon: ${player.weapon}</p>`)
+      <p>species: ${player.species}</p> <p>health: ${player.health}</p>
+      <p>Vigor: ${player.vigor}</p>
+      <p>Potency: ${player.potency}</p>
+      <p>Richesse: ${player.richesse}</p>
+      <p>Pomposity: ${player.pomposity}</p>
+      <p>Social Grace: ${player.social_grace}</p>
+      <p>weapon: ${player.weapon}</p>`);
   }
 
 battle.displayStats = function (){
       $("#player_stats").html(`<p>adventurer: ${player.name}</p> <p>class: ${player.class.name}</p>
-      <p>species: ${player.species}</p> <p>health: ${player.health}</p> <p>strength: ${player.strength}</p>
+      <p>species: ${player.species}</p> <p>health: ${player.health}</p>
+      <p>Vigor: ${player.vigor}</p>
+      <p>Potency: ${player.potency}</p>
+      <p>Richesse: ${player.richesse}</p>
+      <p>Pomposity: ${player.pomposity}</p>
+      <p>Social Grace: ${player.social_grace}</p>
       <p>weapon: ${player.weapon}</p>`);
        $("#enemy_stats").html(`<p> enemy: ${enemy.name} </p> <p> class: ${enemy.class.name}</p> <p> species: ${enemy.species}</p>
       <p> health: ${enemy.health} </p>  <p> strength: ${enemy.strength} </p> <p>weapon: ${enemy.weapon}</p>`);
@@ -27,7 +37,13 @@ battle.enemySetup = function() {
     enemy.generateWeapon();
     console.log("enemy", enemy)
     $("#enemy_stats").html(`<p> enemy: ${enemy.name} </p> <p> class: ${enemy.class.name}</p> <p> species: ${enemy.species}</p>
-      <p> health: ${enemy.health} </p>  <p> strength: ${enemy.strength} </p> <p>weapon: ${enemy.weapon}</p>`);
+      <p>health: ${enemy.health} </p>
+      <p>Filth: ${enemy.filth}</p>
+      <p>Poverty: ${enemy.poverty}</p>
+      <p>Hunger: ${enemy.hunger}</p>
+      <p>Social Disease: ${enemy.social_disease}</p>
+      <p>Damnedness: ${enemy.damnedness}</p>
+      <p>weapon: ${enemy.weapon}</p>`);
 
 }
 //Have a button with the label "Attack".
