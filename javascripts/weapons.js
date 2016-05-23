884926bb4745d0827c2de187862d08171af06e0a
Mark Easterling <marke.easterling@gmail.com>
2016-05-23 11:31:16 -0500

    added more insults

--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -44,8 +44,11 @@ var Gauntlet = (function(ouch) {
     ouch.your_Mother = function(){
       this.name = "Attacks on mother";
       this.damage = 10;
-      this.phrases = ["'Your mother indulges in drink with such reckless abandon her ruddy cheeks could light the whole of London of a night'.",
-                      "'Your mother is so corpulent her boudoir must be stocked with mended ship sails'!"];
+      this.phrases = ['"Your mother indulges in drink with such reckless abandon her ruddy cheeks could light the whole of London of a night".',
+                      '"Your mother is so corpulent her boudoir must be stocked with mended ship sails"!',
+                      '"Your mother’s appetite is so indefatigable that she finds famine in the most decadent of feasts"!',
+                      '"Your mother’s morals are so unscrupulous that the whole of London’s poor could be clothed with the abandoned garments of her suitors"!',
+                      '"your mother’s phrenological examination warranted the doctor checking her arse for a tail that he might have missed"!'];
 
     };
     ouch.your_Mother.prototype = new ouch.Weapon();
@@ -55,7 +58,8 @@ var Gauntlet = (function(ouch) {
       this.damage = 15;
       this.phrases = ['"May a thousand syphalitic camels spit in your cous cous"',
                       '"May the fleas of a thousand dogs color your smallclothes"',
-                      '"May the flies of a thousand horse droppings land in your stew"'];
+                      '"May the flies of a thousand horse droppings land in your stew"',
+                      '"May your wits be so dim that you cannot steal oxygen from the very air you breathe"!'];
     };
     ouch.Curses.prototype = new ouch.Weapon();
 
@@ -63,10 +67,11 @@ var Gauntlet = (function(ouch) {
       this.name = "Attacks on character";
       this.damage = 5;
       this.phrases = ["'Thou loathed issue of thy father's loins'!",
-                      "'Your hygiene is so utterly disastrous, your approach looked like the onset of an Arabian sandstorm'!",
-                      "'Step down. You are not even worthy to cross the path of my concubine'!",
+                      '"Your hygiene is so utterly disastrous, your approach looked like the onset of an Arabian sandstorm"!',
+                      '"Step down. You are not even worthy to cross the path of my concubine"!',
                       '"Avert your glare before it sullies my garments"!',
-                      '"Fetch me your rucksack so my man may thrash you with it"!'];
+                      '"Fetch me your rucksack so my man may thrash you with it"!',
+                      '"Should your offspring be any more wretched than you, biologists may well suspect the human species is stumbling drunkenly back from whence it came"!'];
     };
     ouch.character_Attacks.prototype = new ouch.Weapon();
 
